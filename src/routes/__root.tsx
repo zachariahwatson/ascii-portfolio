453893720a738df144d90d88dc01a796be3add2e
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2026-03-01 18:08:43 -0600

    add head + tweak borders

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,29 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { HeadContent, Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 import { ASCII, ASCIIProvider } from 'html-to-ascii'
 import Footer from '@/components/Footer'
 
 export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        title: 'zachariah-watson.txt',
+      },
+    ],
+    links: [
+      {
+        rel: 'icon',
+        href: '/favicon.ico',
+      },
+    ],
+  }),
   component: () => (
     <>
-      <ASCIIProvider>
+      <head>
+        <HeadContent />
+      </head>
+      <ASCIIProvider tl="╭" tr="╮" br="╯" bl="╰" sl="░" sr="░" st="░" sb="░">
         <ASCII>
           <Footer />
           <Outlet />
