aa8da72424987d3df0b5f364e0d082edc5b29969
zachariahwatson <watsonzachariah@gmail.com>
2026-03-21 23:27:27 -0500

    add contact and desc

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -42,30 +42,96 @@ _/   |_/_)(_)(_(_(__
 `}
         </div>
       </Banner>
-      <div className="whitespace-pre ascii-text">{String.raw`
-   _______________________________________
-  |\_____________________________________/|
-  ||                                     ||
-  ||                                     ||
-  ||                                     ||
-  ||                                     ||
-  ||                                     ||
-  ||                                     ||
-  ||                                     ||
-  ||                                     ||
-  ||                                     ||
-  ||                                     ||
-  ||                                     ||
-  ||                                     ||
-  ||                                     ||
-  ||                                     ||
-  ||                                     ||
-  ||                                     ||
-  ||                                     ||
-  ||_____________________________________||
-  '/_____________________________________\'
-`}</div>
-      <img src="./portrait.png"></img>
+      <div className="flex justify-center items-center">
+        <div
+          className="relative whitespace-pre ascii-text flex flex-row px-8"
+          style={{
+            width:
+              grid.windowWidth >= queries.xxl
+                ? queries.xl
+                : grid.windowWidth >= 1080
+                  ? queries.lg
+                  : queries.md,
+          }}
+        >
+          {/* ASCII frame */}
+          {String.raw`
+   _______________________________
+  |\_____________________________/|
+  ||                             ||
+  ||                             ||
+  ||                             ||
+  ||                             ||
+  ||                             ||
+  ||                             ||
+  ||                             ||
+  ||                             ||
+  ||                             ||
+  ||                             ||
+  ||                             ||
+  ||                             ||
+  ||                             ||
+  ||                             ||
+  ||                             ||
+  ||                             ||
+  ||                             ||
+  ||_____________________________||
+  '/_____________________________\'
+`}
+          {/* <img
+            src="./portrait.png"
+            className="absolute inset-0 w-[37ch] h-full object-cover pl-16"
+          /> */}
+          <div className="flex-1 whitespace-pre-wrap pt-8 pl-4 break-keep">
+            <p className="ascii-text">
+              Zachariah "Zach" Watson is a web developer.
+            </p>
+            <br />
+            <p className="ascii-text">
+              Zachariah "Zach" Watson is an algorithmic artist.
+            </p>
+            <br />
+            <p className="ascii-text">
+              Zachariah "Zach" Watson is a singer-songwriter and producer.
+            </p>
+            <br />
+            <p className="ascii-text">
+              Zachariah "Zach" Watson is{' '}
+              <span className="ascii-underline ascii-text">not</span>{' '}
+              subliminally reinforcing his name in your memory.
+            </p>
+            <div className="ascii-text whitespace-pre">
+              {String.raw`
+┏━╸┏━┓┏┓╻╺┳╸┏━┓┏━╸╺┳╸
+┃  ┃ ┃┃┗┫ ┃ ┣━┫┃   ┃ 
+┗━╸┗━┛╹ ╹ ╹ ╹ ╹┗━╸ ╹
+---------------------
+`}
+            </div>
+            <br />
+            <div className="flex flex-wrap space-x-8">
+              <a
+                href="mailto:zachariahwatson@gmail.com"
+                className="ascii-text ascii-underline"
+              >
+                email
+              </a>
+              <a
+                href="https://instagram.com/zchwtsn"
+                className="ascii-text ascii-underline"
+              >
+                instagram
+              </a>
+              <a
+                href="https://x.com/zchwtsn"
+                className="ascii-text ascii-underline"
+              >
+                twitter
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
