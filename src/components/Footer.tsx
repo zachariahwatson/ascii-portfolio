8ff450e4437ff4111ca5eab45372d14270533873
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2026-04-04 14:25:47 -0500

    cleanup

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,6 @@ export default function Footer() {
     <footer>
       <div className="w-full text-center ascii-text ascii-no-fill">
         © 2026 Zachariah Watson
-        {/* <a
-          href="https://github.com/zachariahwatson/html-to-ascii"
-          className="ascii-text"
-          target="_blank"
-        >
-          html-to-ascii v0.6.2
-        </a> */}
       </div>
     </footer>
   )
