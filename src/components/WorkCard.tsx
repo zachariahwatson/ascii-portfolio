4203c2a6d4bc1f7663d7904890d532d8a851ee88
zachariahwatson <watsonzachariah@gmail.com>
2026-04-04 15:34:59 -0500

    margin troubles

--- src/components/WorkCard.tsx
+++ src/components/WorkCard.tsx
@@ -45,7 +45,7 @@ export default function WorkCard({
       <div className="flex flex-col justify-start h-full relative">
         <div className="ascii-text ascii-no-fill">{children}</div>
         <div className="px-4 mt-4 ascii-text ascii-no-fill flex-1">{desc}</div>
-        <div className="px-4 mt-6 ascii-text ascii-no-fill">{tags}</div>
+        <div className="px-4 mt-4 ascii-text ascii-no-fill">{tags}</div>
         <div className="flex justify-center md:space-x-10 space-x-8 justify-self-end pb-9 mt-1">
           {website && (
             <ButtonLink
