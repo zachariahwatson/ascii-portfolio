1c696f280de0c9525bf17212fa79ea04789bc140
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2026-04-05 20:59:00 -0500

    Added text glow for a terminal effect

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -27,6 +27,18 @@ export const Route = createRootRoute({
         st="░"
         sb="░"
         fontPath={`${import.meta.env.BASE_URL}CascadiaMono-VariableFont_wght.ttf`}
+        //     fontColor="#fff609"
+        //     bgColor="#150c05"
+        //     textShadow="0 0 10px #ff8000, 0 0 24px #ff8000, 0 0 32px #ff8000,
+        // 0 0 6px #ff4540, 0 0 0px #ff4540, 0 0 0px #ff4540"
+        //     bgColor="#00140b"
+        //     fontColor="#00f193"
+        //     textShadow="0 0 10px #01c066, 0 0 24px #01c066, 0 0 4px #01c066,
+        // 0 0 5px #0713a9, 0 0 0px #0713a9, 0 0 0px #0713a9"
+        bgColor="#0e0e0e"
+        fontColor="#fffefe"
+        textShadow="0 0 10px #b9f8ff, 0 0 24px #b9f8ff, 0 0 2px #b9f8ff,
+    0 0 10px #0713a9, 0 0 0px #0713a9, 0 0 0px #0713a9"
       >
         <ASCII>
           <div className="min-h-screen">
